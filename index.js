479473ac3d6e897f1bd1324ae701bf08e63ce963
hotdog <hotdog929@gmail.com>
2016-02-14 22:29:50 +0800

    v0.0.1

---
+++ index.js
@@ -0,0 +1,118 @@
+var Q = require('q');
+var es = require('event-stream');
+
+function dirPath(path){
+    var result = path.trim().match(/^\/?(.*?)\/?$/)[1];
+    return (result.length == 0) ? result : (result + '/');
+}
+
+function filePath(path){
+    return path.trim().match(/^\/?(.*?)\/?$/)[1];
+}
+
+function countPathLayer(path){
+    var dPath = dirPath(path);
+    return (dPath.length == 0) ? 0 : (dPath.split('/').length - 2);
+}
+
+function splitPaths(paths){
+    var pathList = paths.split(',');
+    var list = [];
+    for(var i=0 ; i<pathList.length ; i++){
+        list.push(pathList[i].trim());
+    }
+    return list;
+}
+
+function streamsPromise(){
+    var streams = arguments;
+    var deferred = Q.defer();
+    var endNum = 0;
+    var onStreamEnd = function(){
+        endNum++;
+        if(endNum == streams.length){
+            deferred.resolve();
+        }
+    };
+    var onStreamError = function(){
+        deferred.reject();
+    };
+    for(var i=0 ; i<streams.length ; i++){
+        streams[i]
+            .on('end', onStreamEnd)
+            .on('error', onStreamError)
+    }
+    return deferred.promise;
+}
+
+function streamsFnPromise(){
+    var fns = arguments;
+    var streams = [];
+    for(var i=0 ; i<fns.length ; i++){
+        streams.push(fns[i]());
+    }
+    return streamsPromise.apply(this, streams);
+}
+
+function logStream(fn, args){
+    var deferred = Q.defer();
+    var startTime = new Date().getTime();
+    console.log("Starting '" + fn.name + "'");
+    fn.apply(this, args).on('end', function(){
+            deferred.resolve();
+            console.log("Finished '" + fn.name + "' after " + (new Date().getTime() - startTime) + 'ms');})
+        .on('error', function(error){
+            deferred.reject();
+            console.log("'" + fn.name + "' errored after " + (new Date().getTime() - startTime) + 'ms');});
+    return deferred.promise;
+}
+
+function logPromise(fn, args){
+    var deferred = Q.defer();
+    var startTime = new Date().getTime();
+    console.log("Starting '" + fn.name + "'");
+    fn.apply(this, args).then(function(){
+            deferred.resolve();
+            console.log("Finished '" + fn.name + "' after " + (new Date().getTime() - startTime) + 'ms');})
+        .catch(function(error){
+            deferred.reject();
+            console.log("'" + fn.name + "' errored after " + (new Date().getTime() - startTime) + 'ms');});
+    return deferred.promise;
+}
+
+function replaceEnv(layerNum){
+    var relativePath = '.';
+    if(layerNum > 0){
+        var layerList = [];
+        for(var i=0 ; i<layerNum ; i++){
+            layerList.push('..');
+        }
+        relativePath = layerList.join('/');
+    }
+    return es.map(function(file, cb){
+        var reslut = file.contents.toString().replace(/\$ROOT\$/g, relativePath);
+        file.contents = new Buffer(reslut);
+        cb(null, file);
+    });
+}
+
+
+var result = {
+    dirPath : dirPath,
+    filePath : filePath,
+    countPathLayer : countPathLayer,
+    splitPaths : splitPaths,
+    streamsPromise : streamsPromise,
+    streamsFnPromise : streamsFnPromise,
+    logStream : logStream,
+    logPromise : logPromise,
+    replaceEnv : replaceEnv
+};
+
+if(typeof(window) != 'undefined' && window != null){
+    window['gulp-template-util'] = result;
+}
+
+if(typeof(module) != 'undefined' && module != null){
+    module.exports = result;    
+}
---
+++ test/test.js
@@ -0,0 +1,140 @@
+var assert = require('assert');
+var EventEmitter = require('events');
+var Q = require('q');
+var es = require('event-stream');
+var util = require('../index');
+
+function createSuccessStream(){
+	var emitter = new EventEmitter();
+	setTimeout(function(){
+		emitter.emit('end');
+	}, Math.random() * 200);
+	return emitter;
+}
+
+function createFailStream(){
+	var emitter = new EventEmitter();
+	setTimeout(function(){
+		emitter.emit('error');
+	}, Math.random() * 200);
+	return emitter;
+}
+
+function createStream(success){
+	return (success) ? createSuccessStream() : createFailStream();
+}
+
+describe('gulp-template-util', function() {
+	it('#dirPath()', function(){
+		assert.equal(util.dirPath('src/main/index'), 'src/main/index/');
+		assert.equal(util.dirPath('src/main/index/'), 'src/main/index/');
+		assert.equal(util.dirPath('/src/main/index'), 'src/main/index/');
+		assert.equal(util.dirPath('/src/main/index/'), 'src/main/index/');
+		assert.equal(util.dirPath('src'), 'src/');
+		assert.equal(util.dirPath('src/'), 'src/');
+		assert.equal(util.dirPath('/src'), 'src/');
+		assert.equal(util.dirPath('/src/'), 'src/');
+		assert.equal(util.dirPath('/'), '');
+		assert.equal(util.dirPath(''), '');
+	});
+	it('#filePath()', function(){
+		assert.equal(util.filePath('src/main/index'), 'src/main/index');
+		assert.equal(util.filePath('src/main/index/'), 'src/main/index');
+		assert.equal(util.filePath('/src/main/index'), 'src/main/index');
+		assert.equal(util.filePath('/src/main/index/'), 'src/main/index');
+		assert.equal(util.filePath('src'), 'src');
+		assert.equal(util.filePath('src/'), 'src');
+		assert.equal(util.filePath('/src'), 'src');
+		assert.equal(util.filePath('/src/'), 'src');
+		assert.equal(util.filePath('/'), '');
+		assert.equal(util.filePath(''), '');
+	});
+	it('#countPathLayer()', function(){
+		assert.equal(util.countPathLayer('src/main/index'), 2);
+		assert.equal(util.countPathLayer('src/main/index/'), 2);
+		assert.equal(util.countPathLayer('/src/main/index'), 2);
+		assert.equal(util.countPathLayer('/src/main/index/'), 2);
+		assert.equal(util.countPathLayer('src/main'), 1);
+		assert.equal(util.countPathLayer('src/main/'), 1);
+		assert.equal(util.countPathLayer('/src/main'), 1);
+		assert.equal(util.countPathLayer('/src/main/'), 1);
+		assert.equal(util.countPathLayer('src'), 0);
+		assert.equal(util.countPathLayer('src/'), 0);
+		assert.equal(util.countPathLayer('/src'), 0);
+		assert.equal(util.countPathLayer('/src/'), 0);
+		assert.equal(util.countPathLayer('/'), 0);
+		assert.equal(util.countPathLayer(''), 0);
+	});
+	it('#splitPaths()', function(){
+		assert.deepEqual(util.splitPaths("src/main/index,src/main/site1,src/main,src"), ["src/main/index","src/main/site1","src/main","src"]);
+		assert.deepEqual(util.splitPaths("src/main/index , src/main/site1 , src/main , src"), ["src/main/index","src/main/site1","src/main","src"]);
+		assert.deepEqual(util.splitPaths("src/main/index,src"), ["src/main/index","src"]);
+		assert.deepEqual(util.splitPaths("src/main/index , src"), ["src/main/index","src"]);
+		assert.deepEqual(util.splitPaths("src/main/index"), ["src/main/index"]);
+		assert.deepEqual(util.splitPaths(""), [""]);
+	});
+	describe('#streamsPromise()', function(){
+		it('3 stream 3 success', function(){
+			return util.streamsPromise(createStream(true), createStream(true), createStream(true));
+		});
+		it('1 stream 1 success', function(){
+			return util.streamsPromise(createStream(true));
+		});
+		it('3 stream 2 success 1 fail', function(){
+			var deferred = Q.defer();
+			util.streamsPromise(createStream(true), createStream(false), createStream(true))
+				.then(null, function(){
+					deferred.resolve();
+				});
+			return deferred.promise;
+		});
+		it('1 stream 1 fail', function(){
+			var deferred = Q.defer();
+			util.streamsPromise(createStream(false))
+				.then(null, function(){
+					deferred.resolve();
+				});
+			return deferred.promise;
+		});
+	});
+	describe('#streamsFnPromise()', function(){
+		it('3 stream 3 success', function(){
+			return util.streamsFnPromise(createSuccessStream, createSuccessStream, createSuccessStream);
+		});
+		it('1 stream 1 success', function(){
+			return util.streamsFnPromise(createSuccessStream);
+		});
+		it('3 stream 2 success 1 fail', function(){
+			var deferred = Q.defer();
+			util.streamsFnPromise(createSuccessStream, createFailStream, createSuccessStream)
+				.then(null, function(){
+					deferred.resolve();
+				});
+			return deferred.promise;
+		});
+		it('1 stream 1 fail', function(){
+			var deferred = Q.defer();
+			util.streamsFnPromise(createFailStream)
+				.then(null, function(){
+					deferred.resolve();
+				});
+			return deferred.promise;
+		});
+	});
+	describe('#replaceEnv()', function(){
+		function createPathIt(layerNum, relativePath) {
+			var deferred = Q.defer();
+			es.readArray([{contents : new Buffer('import("$ROOT$/env")')}])
+				.pipe(util.replaceEnv(layerNum))
+				.pipe(es.map(function(file, cb){
+					assert.equal(file.contents.toString(), 'import("' + relativePath + '/env")');
+					deferred.resolve();
+				}));
+			return deferred.promise;
+		};
+		it('. path', function(){return createPathIt(0, '.');});
+		it('.. path', function(){return createPathIt(1, '..');});
+		it('../.. path', function(){return createPathIt(2, '../..');});
+		it('../../.. path', function(){return createPathIt(3, '../../..');});
+	});
+});
